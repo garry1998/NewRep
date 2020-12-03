061dd690b269889303d30bd12ff41fa90a19c597
garry1998 <grvcindia34@gmail.com>
2020-12-03 12:26:56 +0530

    Code2

---
+++ WebApplication65/PayScaleDbContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace WebApplication65
+{
+    public partial class PayScaleDbContext : DbContext
+    {
+        public PayScaleDbContext()
+        {
+        }
+
+        public PayScaleDbContext(DbContextOptions<PayScaleDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Scale> Scales { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=LAPTOP-1KL3BT26;Database=PayScaleDb;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Scale>(entity =>
+            {
+                entity.HasKey(e => e.Payband)
+                    .HasName("PK__Scale__7852CA5A87C2B0A7");
+
+                entity.ToTable("Scale");
+
+                entity.Property(e => e.Payband)
+                    .HasMaxLength(10)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ WebApplication65/Scale.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace WebApplication65
+{
+    public partial class Scale
+    {
+        public string Payband { get; set; }
+        public double? PaySalary { get; set; }
+    }
+}
